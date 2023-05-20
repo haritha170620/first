5bc33c541a4dc246eda095fefedf46be204e2c3e
haritha170620 <haritha.170620@gmail.com>
2023-05-20 13:46:20 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def srujana(request):
+    return HttpResponse('<h1>srujana tinnavara vadhilesthunavaa.....</h1>')
+def revi(request):
+    return HttpResponse('<h1>mamakutty long drive polamaaa..</h1>')
